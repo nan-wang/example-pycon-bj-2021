76481a0e6bb48f60b78059edc8e3498a65db751f
Nan Wang@Home.MBP <Nan.Wang.28@gmail.com>
2021-12-24 20:37:23 +0800

    feat: add cross modal demo

---
+++ crossmodal_search/streamlit.py
@@ -0,0 +1,149 @@
+import os
+
+import streamlit as st
+import requests
+import numpy as np
+import cv2
+from math import ceil
+import tempfile
+
+DEFAULT_ENDPOINT = 'http://127.0.0.1:45678/search'
+
+APP_NAME = 'Cross Modal Search'
+APP_DESCRIPTION = 'Looking For Images From Captions and vice versa'
+
+TOP_K = 10
+
+
+# Query For Data
+def request_and_display(query_input, top_k):
+    headers = {
+        'Content-Type': 'application/json',
+    }
+
+    doc = {}
+    search_type = 0
+    if query_input.endswith('.jpg') and os.path.exists(query_input):
+        doc['uri'] = query_input
+        doc['mime_type'] = 'image/jpeg'
+    else:
+        doc['text'] = str(query_input)
+        doc['mime_type'] = 'text/plain'
+        search_type = 1
+
+    body = {
+        'data': [doc],
+        'parameters': {
+            'limit': top_k
+        }
+    }
+    response = requests.post(DEFAULT_ENDPOINT, headers=headers, json=body)
+    content = response.json()
+    matches = content['data']['docs'][0]['matches']
+    display_photos(matches, search_type)
+
+
+# Display match photos:
+def display_photos(matches, search_type):
+    st.markdown(
+        f"""
+        ### Best matches for your query:
+        """
+    )
+    cnt = len(matches)
+    row = int(ceil(cnt / 2))
+    for i in range(row):
+        col1, col2 = st.columns(2)
+        with col1:
+            if i * 2 < cnt:
+                # search_type 1:search with text  0:search with image
+                if search_type:
+                    uri = matches[i * 2]['uri']
+                else:
+                    uri = matches[i * 2]['uri']
+                score = matches[i * 2]["scores"]['cosine']['value']
+                st.markdown(f"#### No.{i * 2 + 1}: Score  {score:.3f}\n ")
+                st.image(uri)
+        with col2:
+            if i * 2 + 1 < cnt:
+                # search_type 1:search with text  0:search with image
+                if search_type:
+                    uri = matches[i * 2 + 1]['uri']
+                else:
+                    uri = matches[i * 2 + 1]['uri']
+                score = matches[i * 2 + 1]['scores']['cosine']['value']
+                st.markdown(f"####  No.{i * 2 + 2}:Score  {score:.3f}\n ")
+                st.image(uri)
+
+
+def main():
+    # Header
+    st.markdown(
+        f"""
+        # <a href="https://github.com/jina-ai/jina/">Jina</a>\'s {APP_NAME}
+        ### {APP_DESCRIPTION}
+        """,
+        unsafe_allow_html=True,
+    )
+
+    # Sidebar
+    st.sidebar.header('Looking For Images From Captions and vice versa')
+    input_choice = st.sidebar.selectbox(
+        'Please choose the type of your input:',
+        [{'name': 'search_by_text', 'display': 'input text and search for photos'},
+         {'name': 'search_by_image', 'display': 'upload photo and search for description'}],
+        format_func=lambda x: x['display']
+    )['name']
+    settings = st.sidebar.expander(label='Settings', expanded=False)
+    with settings:
+        endpoint = st.text_input(label='Endpoint', value=DEFAULT_ENDPOINT)
+        top_k = st.number_input(label='Top K', value=TOP_K, step=1)
+    st.sidebar.button('select')
+
+    st.sidebar.markdown(
+        f"""
+        **This is a {APP_NAME} using the [Jina neural search framework](https://github.com/jina-ai/jina/).**
+        You can search for photos using description or getting your own photo's description!
+
+        <a href="https://github.com/jina-ai/jina/"><img src="https://github.com/alexcg1/jina-app-store-example/blob/a8f64332c6a5b3ae42df07d4bd615ff1b7ece4d9/frontend/powered_by_jina.png?raw=true" width=256></a>
+        """,
+        unsafe_allow_html=True,
+    )
+
+    query_text = None
+    query_image = None
+    query_image_file = None
+    # Content
+    if input_choice == 'search_by_text':
+        query_text = st.text_input(
+            label='please input your search content'
+        )
+    elif input_choice == 'search_by_image':
+        query_image = st.file_uploader(
+            label='select your JPEG image',
+            type="jpg"
+        )
+        if query_image is not None:
+            query_image_file = tempfile.NamedTemporaryFile(delete=False, suffix='.jpg')
+
+            # change the file into openCV mode
+            file_bytes = np.asarray(bytearray(query_image.read()), dtype=np.uint8)
+            opencv_image = cv2.imdecode(file_bytes, 1)
+
+            # display
+            st.image(opencv_image, channels="BGR")
+            # store in certain directory
+            cv2.imwrite(query_image_file.name, opencv_image)
+
+    clicked = st.button("Search")
+    if clicked:
+        if (not query_text) and (not query_image_file):
+            st.markdown('Please enter a query')
+        else:
+            request_and_display(query_text or query_image_file.name, top_k)
+            if query_image_file:
+                query_image_file.close()
+
+
+if __name__ == '__main__':
+    main()
